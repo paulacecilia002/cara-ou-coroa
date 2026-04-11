b0b07645a1f79a6daaccee9d5d9da5a3f8e88e00
Paula Cecilia <paulacecilia4700@gmail.com>
2026-04-11 15:34:47 -0300

    subindo projeto 2

---
+++ lib/Jogar.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Jogar extends StatefulWidget {
+  const Jogar({super.key});
+
+  @override
+  State<Jogar> createState() => _JogarState();
+}
+
+class _JogarState extends State<Jogar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ,
+      body: ,
+    );
+  }
+}
