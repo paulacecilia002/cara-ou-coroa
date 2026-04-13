10e718fe7a4a6afc09de9a3677fb69e3e11fdf96
Paula Cecilia <paulacecilia4700@gmail.com>
2026-04-13 18:39:29 -0300

    colocando prefixo de 0xff para uma cor hexadecimal

--- lib/Jogar.dart
+++ lib/Jogar.dart
@@ -11,7 +11,7 @@ class _JogarState extends State<Jogar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: Color(0xff9C27F5),
       body: Container(
 
       ),
